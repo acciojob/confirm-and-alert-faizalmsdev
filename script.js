390a6417fee00ee337ab0bf5cf45030f79dc75d7
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-07-03 20:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Ask for confirmation
+var proceed = confirm("Do you want to proceed?");
+
+// Check the user's response
+if (proceed) {
+    // If OK is clicked
+    alert("You clicked OK. Proceeding...");
+} else {
+    // If Cancel is clicked
+    alert("You clicked Cancel. Exiting...");
+}
+
